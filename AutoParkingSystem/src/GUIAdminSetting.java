315d6a2e0c5b5622a21976955ebd36a07ed15bd6
ckqn06 <ckqn06@naver.com>
2022-05-16 13:05:48 +0900

    수정한 버전입니다. 앞으로 이렇게 수정할 예정입니다.

--- AutoParkingSystem/src/GUIAdminSetting.java
+++ AutoParkingSystem/src/GUIAdminSetting.java
@@ -1,95 +1,85 @@
-
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
 
 public class GUIAdminSetting extends JFrame{
 	private JPanel p = new JPanel();
-	
-    private JButton SettingButton = new JButton("설정 변경"); // 설정 버튼
-    private JButton goMainButton = new JButton("메인화면으로"); // 메인화면으로 버튼
-    
-    private Font font = new Font("고딕", Font.BOLD, 50);
+	private Font font = new Font("맑은 고딕", Font.BOLD, 40); //폰트 설정
 
     private JLabel placeLabel = new JLabel("▶ 주차 공간 설정");
-    private JLabel detailLabel = new JLabel("<html>(최소 가로 1칸/세로 1칸<br/>최대 가로 10칸 세로 15칸 설정 가능)</html>");
+    private JLabel detailLabel = new JLabel("<html>(최소 가로 1칸/세로 1칸<br/>최대 가로 10칸/세로 15칸 설정 가능)</html>");
     private JLabel widthLabel = new JLabel("가로 :");
     private JLabel heightLabel = new JLabel("세로 :");
     private JLabel payLabel = new JLabel("▶ 시간당 주차 비용 설정");
 
-    private JTextField widthText = new JTextField(); // 가로 입력 창
-    private JTextField heightText = new JTextField(); // 세로 입력 창
-    private JTextField payText = new JTextField(); // 비용 입력 창
+    private JTextField widthText = new JTextField(); //가로 입력 창
+    private JTextField heightText = new JTextField(); //세로 입력 창
+    private JTextField payText = new JTextField(); //비용 입력 창
+    
+    private JButton SettingButton = new JButton("설정 변경"); //설정 버튼
+    private JButton goMainButton = new JButton("설정"); //메인 화면 버튼
+    
+    File f = new File("C://Server/관리자 데이터 파일.txt");
 
-    GUIAdminSetting(){  // 화면 기본 설정
+    GUIAdminSetting(){  //화면 기본 설정
         this.setTitle("관리자 설정 화면");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.formDesign();
         this.eventListner();
-        this.setSize(1000,1000);
+        this.setSize(1000,800);
         this.setVisible(true);
     }
 
-    private void formDesign() {  // 각 GUI 객체 설정
+    private void formDesign() { //각 GUI 객체 설정
         this.add(p);
         p.setLayout(null);
-
-        SettingButton.setLocation(240, 780);
-        SettingButton.setSize(280, 100);
-        SettingButton.setFont(font);
         
-        goMainButton.setLocation(550, 780);
-        goMainButton.setSize(400, 100);
-        goMainButton.setFont(font);
-        
-        placeLabel.setLocation(100, 70);
+        placeLabel.setLocation(100, 20);
         placeLabel.setSize(600, 100);
         placeLabel.setHorizontalAlignment(SwingConstants.LEFT);
         placeLabel.setFont(font);
 
-        detailLabel.setLocation(100, 100);
+        detailLabel.setLocation(100, 50);
         detailLabel.setSize(1000, 200);
         detailLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        detailLabel.setFont(new Font("고딕", Font.PLAIN, 40));
+        detailLabel.setFont(new Font("고딕", Font.PLAIN, 30));
         
-        widthLabel.setLocation(120, 280);
+        widthLabel.setLocation(120, 205);
         widthLabel.setSize(500, 100);
         widthLabel.setHorizontalAlignment(SwingConstants.LEFT);
         widthLabel.setFont(font);
         
-        heightLabel.setLocation(120, 410);
+        heightLabel.setLocation(120, 315);
         heightLabel.setSize(500, 100);
         heightLabel.setHorizontalAlignment(SwingConstants.LEFT);
         heightLabel.setFont(font);
         
-        payLabel.setLocation(100, 540);
+        payLabel.setLocation(100, 430);
         payLabel.setSize(800, 100);
         payLabel.setHorizontalAlignment(SwingConstants.LEFT);
         payLabel.setFont(font);
 
-        widthText.setLocation(300, 280);
-        widthText.setSize(400, 100);
+        widthText.setLocation(270, 220);
+        widthText.setSize(400, 80);
         widthText.setFont(font);
 
-        heightText.setLocation(300, 410);
-        heightText.setSize(400, 100);
+        heightText.setLocation(270, 330);
+        heightText.setSize(400, 80);
         heightText.setFont(font);
         
-        payText.setLocation(110, 640);
-        payText.setSize(400, 100);
+        payText.setLocation(110, 530);
+        payText.setSize(400, 80);
         payText.setFont(font);
+        
+        SettingButton.setLocation(400, 650);
+        SettingButton.setSize(280, 80);
+        SettingButton.setFont(font);
+        
+        goMainButton.setLocation(400, 650);
+        goMainButton.setSize(300, 80);
+        goMainButton.setFont(font);
 
-
-        p.add(SettingButton);
-        p.add(goMainButton);
         p.add(placeLabel);
         p.add(detailLabel);
         p.add(widthLabel);
@@ -98,41 +88,68 @@ public class GUIAdminSetting extends JFrame{
         p.add(widthText);
         p.add(heightText);
         p.add(payText);
+        
+        if(f.exists()) {
+        	p.add(SettingButton);
+        } else {
+        	p.add(goMainButton);
+        }
     }
 
-    private void eventListner() { // 버튼 클릭 이벤트 설정
-    		SettingButton.addActionListener(new ActionListener() { // 설정 버튼 클릭 시 실행
-    			 public void actionPerformed(ActionEvent e) {
-    				 if(false) { // 현재 주차장에 차가 주차되어있으면 실행
-    					 JOptionPane.showMessageDialog(null, "현재 고객이 존재하여 설정 변경이 불가능합니다");
-    				 }
-    				 
-    				 if(true) { // 숫자만 쓰여져 있는지 확인
-    					 if(true) { // 범위 안의 수인지 확인
-    						 // 설정 파일에 가로, 세로, 비용 값 입력
-    						 // 메인화면에 반영
-        	                 JOptionPane.showMessageDialog(null, "변경하신 설정 값이 정상적으로 적용됐습니다"); // 값이 범위를 벗어났을 때 실행
-    						 
-    						 dispose(); 
-    						 
-    						 new GUIMain();  // 메인화면으로
-    					 } else
-        	                 JOptionPane.showMessageDialog(null, "범위 갑 안의 값을 입력해주세요"); // 값이 범위를 벗어났을 때 실행
+    private void eventListner() { //버튼 클릭 이벤트 설정
+    	goMainButton.addActionListener(new ActionListener() { //설정 버튼 클릭 시 실행
+    		public void actionPerformed(ActionEvent e) {
+    			try {
+    				int width = Integer.parseInt(widthText.getText());
+        			int height = Integer.parseInt(heightText.getText());
+        			int pay = Integer.parseInt(payText.getText());
 
-    				 } else 
-    	                 JOptionPane.showMessageDialog(null, "올바른 값을 입력해주세요"); // 숫자 이외의 값이 쓰여 있을 때 실행
-    			 }
-    		});
-    		
-    		goMainButton.addActionListener(new ActionListener() { // 메인화면으로 버튼 클릭 시 실행
-   			 public void actionPerformed(ActionEvent e) {
-   				 dispose();
-   				 
-   				 new GUIMain(); //메인화면으로
-   			 }
-   		});
-        
+        			if(1<=width && width<=10 && 1<=height && height <= 15) { //범위 안의 수인지 확인
+        				OutputStream os = new FileOutputStream(f);
+        				String str = ("가로 값: "+width + "\n세로 값: "+height + "\n시간 당 주차 비용: "+pay );
+        				byte[] by = str.getBytes();
+        				os.write(by);			
+        				
+        				JOptionPane.showMessageDialog(null, "설정 값이 정상적으로 적용됐습니다");
+        				dispose(); 
+        				new GUIMain();  // 메인화면으로
+        			} else { //값이 범위를 벗어났을 때 실행
+        				JOptionPane.showMessageDialog(null, "범위 값 안의 값을 입력해주세요");
+        			}
+    			} catch(Exception e1) {
+    				JOptionPane.showMessageDialog(null, "올바른 값을 입력해주세요");
+    			}
+    		}
+    	});
+    	
+    	SettingButton.addActionListener(new ActionListener() { //설정 변경 버튼 클릭 시 실행
+    		public void actionPerformed(ActionEvent e) {
+    			try {
+    				int width = Integer.parseInt(widthText.getText());
+        			int height = Integer.parseInt(heightText.getText());
+        			int pay = Integer.parseInt(payText.getText());
 
+        			if(1<=width && width<=10 && 1<=height && height <= 15) { //범위 안의 수인지 확인
+        				OutputStream os = new FileOutputStream(f);
+        				String str = ("가로 값: "+width + "\n세로 값: "+height + "\n시간 당 주차 비용: "+pay );
+        				byte[] by = str.getBytes();
+        				os.write(by);
+        				
+        				JOptionPane.showMessageDialog(null, "변경하신 설정 값이 정상적으로 적용됐습니다");
+        				dispose(); 
+        				new GUIMain();  // 메인화면으로
+        			} else { //값이 범위를 벗어났을 때 실행
+        				JOptionPane.showMessageDialog(null, "범위 값 안의 값을 입력해주세요");
+        			}
+        			
+        			if(false) { //현재 주차장에 차가 주차되어있으면 실행
+        				JOptionPane.showMessageDialog(null, "현재 고객이 존재하여 설정 변경이 불가능합니다");
+        			}
+    			} catch(Exception e1) {
+    				JOptionPane.showMessageDialog(null, "올바른 값을 입력해주세요");
+    			}
+    		}
+    	});
     }
     
     public static void main(String[] args) { // 실행 테스트를 위한 코드
--- AutoParkingSystem/src/GUIFirst.java
+++ AutoParkingSystem/src/GUIFirst.java
@@ -1,103 +1,93 @@
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.File;
 
 public class GUIFirst extends JFrame{
 	private JPanel p = new JPanel();
-	
-    private JButton loginButton = new JButton("로그인"); // 로그인 버튼
+	private Font font = new Font("맑은 고딕", Font.BOLD, 45); //폰트 설정
+
+    private JLabel logoLabel = new JLabel("무인 주차 관리 시스템"); //화면 로고
+    private JLabel IDLabel = new JLabel("관리자 ID :"); //ID 메시지
+    private JLabel passwordLabel = new JLabel("비밀번호 :"); //비밀번호 메시지
+
+    private JTextField IDText = new JTextField(); //ID 입력 창
+    private JTextField passwordText = new JTextField(); //비밀번호 입력 창
     
-    private Font font = new Font("고딕", Font.BOLD, 50);
+    private JButton loginButton = new JButton("로그인"); //로그인 버튼
 
-    private JLabel IDLabel = new JLabel("관리자 ID :");
-    private JLabel passwordLabel = new JLabel("비밀번호 :");
-    private JLabel logoLabel = new JLabel("무인 주차 관리 시스템");
+    File f = new File("C://Server/관리자 데이터 파일.txt");
 
-    private JTextField IDText = new JTextField(); // ID 입력 창
-    private JTextField passwordText = new JTextField(); // 비밀번호 입력 창
-
-    GUIFirst(){ // 화면 기본 설정
-        this.setTitle("첫 화면");
+    GUIFirst(){ //화면 기본 설정
+        this.setTitle("무인 주차 관리 시스템");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.formDesign();
         this.eventListner();
-        this.setSize(1000,1000);
+        this.setSize(1000,800);
         this.setVisible(true);
     }
 
-    private void formDesign() {  // 각 GUI 객체 설정
+    private void formDesign() { //각 GUI 객체 설정
         this.add(p);
         p.setLayout(null);
 
-        loginButton.setLocation(400, 780);
-        loginButton.setSize(200, 100);
-        loginButton.setFont(font);
-        
-        logoLabel.setLocation(100, 160);
+        logoLabel.setLocation(100, 100);
         logoLabel.setSize(800, 100);
         logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
         logoLabel.setFont(font.deriveFont(70f));
+        
+        IDText.setSize(400, 100);
+        IDText.setLocation(450, 300);
+        IDText.setFont(font);
 
-        IDLabel.setLocation(0, 380);
+        passwordText.setSize(400, 100);
+        passwordText.setLocation(450, 470);
+        passwordText.setFont(font);
+
+        IDLabel.setLocation(0, 300);
         IDLabel.setSize(400, 100);
         IDLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         IDLabel.setFont(font);
 
-        passwordLabel.setLocation(0, 550);
+        passwordLabel.setLocation(0, 470);
         passwordLabel.setSize(400, 100);
         passwordLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         passwordLabel.setFont(font);
+        
+        loginButton.setLocation(400, 620);
+        loginButton.setSize(200, 100);
+        loginButton.setFont(font);
 
-        IDText.setSize(400, 100);
-        IDText.setLocation(450, 380);
-        IDText.setFont(font);
-
-        passwordText.setSize(400, 100);
-        passwordText.setLocation(450, 550);
-        passwordText.setFont(font);
-
-
-        p.add(loginButton);
         p.add(logoLabel);
-        p.add(IDLabel);
-        p.add(passwordLabel);
         p.add(IDText);
         p.add(passwordText);
+        p.add(IDLabel);
+        p.add(passwordLabel);
+        p.add(loginButton);
     }
 
-    private void eventListner() { // 버튼 클릭 이벤트 설정
-        loginButton.addActionListener(new ActionListener() { // 로그인 버튼 클릭시 실행
+    private void eventListner() { //버튼 클릭 이벤트 설정
+        loginButton.addActionListener(new ActionListener() { //로그인 버튼 클릭시 실행
             @Override
             public void actionPerformed(ActionEvent e) {
-            	if(IDText.getText().equals("admin")  // ID와 비밀번호가 맞으면 실행
-            			&& passwordText.getText().equals("park123")) {
-            		dispose();
-            		
-            		if(false) // 관리자 설정 파일이 있으면 실행
+            	//ID와 비밀번호가 맞다면
+            	if(IDText.getText().equals("admin") && passwordText.getText().equals("park123")) {
+            		if(f.exists()) {
+            			dispose();
             			new GUIMain();
-            		else { // 관리자 설정 파일이 없으면 실행
-            			JOptionPane.showMessageDialog(null, "현재 시스템에 관리자 데이터 파일이 존재하지 않습니다");
-
-        				new GUIFirstSetting();
-            		}
-            	}else 
-                    JOptionPane.showMessageDialog(null, "관리자 ID 혹은 비밀번호가 틀렸습니다"); // ID와 비밀번호가 틀리면 실행
-        	}		
-            	             
+                    } else {
+                    	JOptionPane.showMessageDialog(null, "현재 시스템에 관리자 데이터 파일이 존재하지 않습니다.");
+                    	dispose();
+                    	new GUIAdminSetting();
+                    }
+            	} else { //ID와 비밀번호가 틀리다면
+            		JOptionPane.showMessageDialog(null, "관리자 ID 혹은 비밀번호가 틀렸습니다");
+            	}
+        	}		 	             
         });
-
     }
-	
-	
-	public static void main(String[] args) {  // 실행 테스트를 위한 코드
-		new GUIFirst();
+    
+	public static void main(String[] args) { // 실행 테스트를 위한 코드
+		new GUIAdminLogin();
 	}
 }
