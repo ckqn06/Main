629836ba45936e8356c4767cb6aec921c00dd4ba
ckqn06 <ckqn06@naver.com>
2022-05-17 13:13:17 +0900

    부분 완료. 결제를 완료했다면 결제한 차량과 관련된 데이터를 지워야 함.  주차 비용을 확인하여 그에 맞는 메시지를 출력해야 함.

--- AutoParkingSystem/src/GUIPayCash.java
+++ AutoParkingSystem/src/GUIPayCash.java
@@ -1,16 +1,6 @@
-
-
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
 public class GUIPayCash extends JFrame{
 	private JPanel p = new JPanel();
