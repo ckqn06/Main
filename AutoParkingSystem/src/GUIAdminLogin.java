c0559eb520408cbc7ab044c300309d9d2848fbfa
ckqn06 <ckqn06@naver.com>
2022-05-26 14:46:40 +0900

    소스 코드 정리 및 주석 추가

--- AutoParkingSystem/src/GUIAdminLogin.java
+++ AutoParkingSystem/src/GUIAdminLogin.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.io.File;
 
 public class GUIAdminLogin extends JFrame{
-	private JPanel p = new JPanel();
+	private JPanel p = new JPanel(); //보조 프레임 설정
 	private Font font = new Font("맑은 고딕", Font.BOLD, 45); //폰트 설정
 
     private JLabel logoLabel = new JLabel("무인 주차 관리 시스템"); //화면 로고
@@ -15,29 +15,30 @@ public class GUIAdminLogin extends JFrame{
     private JTextField passwordText = new JTextField(); //비밀번호 입력 창
     
     private JButton loginButton = new JButton("로그인"); //로그인 버튼
-    private JButton BackButton = new JButton("돌아가기"); // 돌아가기 버튼
+    private JButton BackButton = new JButton("돌아가기"); //돌아가기 버튼
     
     private int op; //실행 경로 확인 숫자
+    
     File f = new File("관리자 데이터 파일.txt"); //관리자 데이터 파일
 
     GUIAdminLogin(int op){ //화면 기본 설정, op=1(첫 로그인), op=2(관리자 설정 로그인), op=3(종료하기)
     	this.op = op;
-        this.setTitle("무인 주차 관리 시스템");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.formDesign();
-        this.eventListner();
-        this.setSize(1000,800);
-        this.setVisible(true);
+        this.setTitle("무인 주차 관리 시스템"); //창 타이틀 바 설정
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //x버튼을 눌러 화면 종료
+        this.formDesign(); //각 GUI 객체 설정
+        this.eventListner(); //이벤트를 처리함
+        this.setSize(1000,800); //화면 크기 설정
+        this.setVisible(true); //창을 화면에 보여줄 것임을 설정
     }
 
     private void formDesign() { //각 GUI 객체 설정
-        this.add(p);
-        p.setLayout(null);
+        this.add(p); //보조 프레임 생성
+        p.setLayout(null); //보조 프레임의 레이아웃 설정
         
-        logoLabel.setLocation(100, 100);
-        logoLabel.setSize(800, 100);
-        logoLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        logoLabel.setFont(font.deriveFont(70f));
+        logoLabel.setLocation(100, 100); //위치 설정
+        logoLabel.setSize(800, 100); //크기 설정
+        logoLabel.setHorizontalAlignment(SwingConstants.CENTER); //정렬 위치
+        logoLabel.setFont(font.deriveFont(70f)); //텍스트 폰트 설정
         
         IDLabel.setLocation(0, 300);
         IDLabel.setSize(400, 100);
@@ -57,12 +58,14 @@ public class GUIAdminLogin extends JFrame{
         passwordText.setLocation(450, 470);
         passwordText.setFont(font);
 
-        if(op != 1) { //첫 로그인이 아니라면 로그인 버튼을 오른쪽으로 이동
+        if(op != 1) { //첫 로그인이 아니라면 로그인 버튼을 오른쪽으로 이동 (= 돌아가기 버튼이 생성됨)
         	loginButton.setLocation(600, 620);
             loginButton.setSize(250, 100);
+            
             if(op == 3) //메인 화면에서 시스템 종료 버튼을 클릭한 경우
-            	loginButton.setText("종료하기"); //로그인이 아니라 종료하기로 텍스트를 변경
-        }else { //첫 로그인이라면 로그인 버튼을 중앙으로 이동
+            	loginButton.setText("종료하기"); //"로그인"이 아니라 "종료하기"로 텍스트를 변경
+        
+        }else { //첫 로그인이라면 로그인 버튼을 왼쪽으로 이동 (= 돌아가기 버튼이 없어짐)
         	loginButton.setLocation(400, 620); 
             loginButton.setSize(200, 100);
         }
@@ -72,7 +75,7 @@ public class GUIAdminLogin extends JFrame{
         BackButton.setSize(250, 100);
         BackButton.setFont(font);
 
-        p.add(logoLabel);
+        p.add(logoLabel); //화면 로고를 보조 프레임에 추가함
         p.add(IDLabel);
         p.add(passwordLabel);
         p.add(IDText);
@@ -88,25 +91,25 @@ public class GUIAdminLogin extends JFrame{
             public void actionPerformed(ActionEvent e) {
             	//ID와 비밀번호가 맞다면
             	if(IDText.getText().equals("admin") && passwordText.getText().equals("park123")) {
-            		if(op == 1) { //만약 첫 로그인이라면
-            			if(f.exists()) {
-                			dispose();
-                			new GUIMain();
-                        } else {
+            		if(op == 1) { //만약 시스템을 실행시켜 첫 로그인을 진행하는 경우
+            			if(f.exists()) { //관리자 데이터 파일이 지정 경로에 존재한다면
+                			dispose(); //현재 보여주고 있는 창(프레임)을 닫음
+                			new GUIMain(); //메인 화면으로 이동
+                        } else { //관리자 데이터 파일이 지정 경로에 존재하지 않는다면
                         	JOptionPane.showMessageDialog(null, "현재 시스템에 관리자 데이터 파일이 존재하지 않습니다.");
                         	dispose();
-                        	new GUIAdminSetting();
+                        	new GUIAdminSetting(); //관리자 설정 화면으로 이동
                         }
-            		}else if (op == 2) { //관리자 설정 로그인이라면
+            			
+            		}else if (op == 2) { //관리자 설정 버튼을 통해 온 경우
             			dispose();
             			new GUIAdminSetting();
-            		}else{ //종료하기 화면이라면
-                		int result = JOptionPane.showConfirmDialog(null, "정말로 시스템을 종료하시겠습니까?", "시스템 종료", JOptionPane.YES_NO_OPTION);
-                		if(result == JOptionPane.YES_OPTION)  //Yes를 선택할 경우
-                			System.exit(0);
-                	}
             			
-                    
+            		}else{ //종료하기 버튼을 통해 온 경우
+                		int result = JOptionPane.showConfirmDialog(null, "정말로 시스템을 종료하시겠습니까?", "시스템 종료", JOptionPane.YES_NO_OPTION);
+                		if(result == JOptionPane.YES_OPTION) //Yes를 선택할 경우
+                			System.exit(0); //시스템을 종료시킴
+                	}
             	} else { //ID와 비밀번호가 틀리다면
             		JOptionPane.showMessageDialog(null, "관리자 ID 혹은 비밀번호가 틀렸습니다");
             	}
@@ -121,7 +124,7 @@ public class GUIAdminLogin extends JFrame{
         });
     }
     
-    public static void main(String[] args) { //실행 테스트를 위한 코드
-		new GUIAdminLogin(1); //처음 로그인 화면으로 실행
+    public static void main(String[] args) {
+		new GUIAdminLogin(1); //시스템을 실행시킬 시, 첫 로그인 화면으로 실행
 	}
 }
